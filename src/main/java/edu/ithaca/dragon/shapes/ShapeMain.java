ac9d095522b496f531f431ab44438f788ae1fd3b
PJayJay <pfield@ithaca.edu>
2024-08-30 15:04:53 -0400

    Finsihed main interface

--- src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
+++ src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
@@ -1,10 +1,40 @@
 package edu.ithaca.dragon.shapes;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class ShapeMain {
     
     public static void main(String[] args){
         //Make a list of 5 randomly-sized rectangles and print their area and the largest line that can be drawn through them
         //Allow the user to choose one, double the size of that one, and print them all again
         //Use a loop to repeat the process 5 times
+        Rectangle a=new Rectangle(5,5);
+        Rectangle b=new Rectangle(10,2);
+        Rectangle c=new Rectangle(.5,1);
+        Rectangle d=new Rectangle(15,20);
+        Rectangle e=new Rectangle(2,4);
+        List<Rectangle> listed= Arrays.asList(a,b,c,d,e);
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.println(listed.get(j).calcArea());
+            }
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("Which rectangle do you want to double? 0,1,2,3,4?");
+            int whichOne = myObj.nextInt();
+            if(whichOne==0 || whichOne==1||whichOne==2||whichOne==3||whichOne==4){
+                listed.get(whichOne).doubleSize();
+            }
+            else{
+                System.out.println("That is not an option what is your problem man");
+                while(whichOne!=0 && whichOne!=1&&whichOne!=2&&whichOne!=3&&whichOne!=4){
+                    Scanner newObj = new Scanner(System.in);
+                    System.out.println("Which rectangle do you want to double? 0,1,2,3,4?");
+                    whichOne = newObj.nextInt();
+                }
+            }
+
+        }
     }
 }
