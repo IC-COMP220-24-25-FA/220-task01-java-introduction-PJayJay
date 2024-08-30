047cbcafdda328e7d7954de6cf5abbdcbf5f76b8
PJayJay <pfield@ithaca.edu>
2024-08-30 11:32:27 -0400

    Made triangle and testing class

---
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -0,0 +1,25 @@
+package edu.ithaca.dragon.shapes;
+
+public class Triangle {
+    private int height;
+    private int width;
+
+    public Triangle(int h, int w){
+        height=h;
+        width=w;
+    }
+
+    public double calcArea(){
+
+    }
+
+    public void doubleSize(){
+
+    }
+
+    public double longestLineWithin(){
+
+        
+    }
+    
+}
